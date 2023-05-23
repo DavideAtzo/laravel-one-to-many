9fe4200dd12c1c944a384dbd90573406eb66070a
DavideAtzo <atzo9477@gmail.com>
2023-05-23 15:59:46 +0200

    visualizzazione a schermo type project

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,21 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+    <div class="container">
 
-    <div class="d-flex justify-content-between align-items-center">
-        <div class="row">
-            <div class="details">
-                <h2 class="fs-4 text-secondary my-3">
-                    {{$project->project_title}}
-                </h2>
-                <div>
-                    <img src="{{ asset('storage/' . $project->image) }}" alt="{{ $project->project_title }}">
+        <div class="d-flex justify-content-between align-items-center">
+            <div class="row">
+                <div class="details">
+                    <h2 class="fs-4 text-secondary my-3">
+                        {{ $project->project_title }}
+                    </h2>
+                    <div>
+                        <img src="{{ asset('storage/' . $project->image) }}" alt="{{ $project->project_title }}">
+                    </div>
+                    <p>{{ $project->description }}</p>
+                    @if ($project->type_id)
+                        <h2>Type: {{ $project->type->name }}</h2>
+                    @endif
+                    <p>{{ $project->creation_date }}</p>
                 </div>
-                <p>{{$project->description}}</p>
-                <p>{{$project->creation_date}}</p>
             </div>
         </div>
     </div>
-</div>
 @endsection
